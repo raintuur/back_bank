b402f19deaae486edb8b8d59e33c37bf867af857
Rain Tüür <rain.tuur@gmail.com>
2022-11-14 14:18:40 +0200

    develop

--- src/main/java/ee/valiit/back_bank/HelloWorldController.java
+++ src/main/java/ee/valiit/back_bank/HelloWorldController.java
@@ -1,2 +1,14 @@
-package ee.valiit.back_bank;public class HelloWorldController {
+package ee.valiit.back_bank;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello/world")
+    public String helloWorld() {
+        return "HELLO WOOOOORLD!!!! YEEE!!! JAVA, YESS, NII ÄGE";
+    }
+
 }
